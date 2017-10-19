6b9ecb25dcb39373b98cfeceb5d816d722be4bf7
James Bennett <jawbenne@iu.edu>
2017-10-19 14:27:04 +0100

    Fixed a few bugs in demo scripts

--- docker-for-devs/demo/reset.sh
+++ docker-for-devs/demo/reset.sh
@@ -4,7 +4,8 @@ docker stack rm todoapp
 
 docker swarm leave -f
 
-rm ~/projects/statewide-iu-2017-docker/Dockerfile
-rm ~/projects/statewide-iu-2017-docker/docker-stack.yml
+rm ~/projects/statewide-it-2017-docker/java/.dockerignore
+rm ~/projects/statewide-it-2017-docker/java/Dockerfile
+rm ~/projects/statewide-iu-2017-docker/java/docker-stack.yml
 
 docker pull jwbennet/todoapp-java:mysql
